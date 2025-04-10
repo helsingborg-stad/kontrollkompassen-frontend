d9f8410b611b03aa06e30c6689144b8ea0e5e6da
MrSpex <mail@dannilsson.se>
2025-04-10 14:19:34 +0200

    🎨fix(controller): cleanup comments

--- frontend/app/source/php/Controller/BaseController.php
+++ frontend/app/source/php/Controller/BaseController.php
@@ -11,17 +11,6 @@ abstract class BaseController
   protected $action = null;
   protected AbstractServices $services;
 
-  /**
-   * Constructor for the class.
-   *
-   * Initializes the object by setting up action listeners, triggering specific actions
-   * based on the provided child class, handling global actions, retrieving manifest data,
-   * and checking if the user is authenticated.
-   *
-   * @param mixed $child The child class instance.
-   *
-   * @return void
-   */
   public function __construct(string $child, AbstractServices $services)
   {
     $this->services = $services;
@@ -64,45 +53,17 @@ abstract class BaseController
     }
   }
 
-  /**
-   * Perform user logout action.
-   *
-   * This function initiates the user logout process by calling the static `logout` method
-   * of the User class. It then creates a new Redirect instance to redirect the user to
-   * the home page with an additional query parameter ('action' => 'logoutmsg') to indicate
-   * a successful logout.
-   *
-   * @return void
-   */
   public function actionLogout()
   {
     $this->services->getSessionService()->endSession();
     new Redirect('/', ['action' => 'logoutmsg']);
   }
 
-  /**
-   * Get the data as an array.
-   *
-   * This function returns the data property as an associative array.
-   *
-   * @return array The data as an associative array.
-   */
   public function getData(): array
   {
     return (array) $this->data;
   }
 
-  /**
-   * Initialize action listener based on the 'action' parameter in the query string.
-   *
-   * This function checks if the 'action' parameter is set in the $_GET array. If set,
-   * it cleans up the action parameter by removing spaces and converting it to a camel-case
-   * format. The cleaned action is then assigned to the class property $this->action.
-   * If the 'action' parameter is not set, $this->action is set to false.
-   *
-   * @return string|false Returns the cleaned and formatted action if 'action' is set,
-   *                     otherwise returns false.
-   */
   public function initActionListener()
   {
     if (isset($_GET['action'])) {
@@ -111,17 +72,6 @@ abstract class BaseController
     return $this->action = false;
   }
 
-
-  /**
-   * Retrieve assets from the manifest file.
-   *
-   * This function reads the manifest file located at BASEPATH/assets/dist/manifest.json
-   * and returns the decoded contents as an associative array. If the file doesn't exist
-   * or cannot be decoded, it returns false.
-   *
-   * @return array|false Returns an associative array containing the assets from the manifest
-   *                   file, or false if the file doesn't exist or cannot be decoded.
-   */
   public function getAssets()
   {
     $revManifest = rtrim(BASEPATH, "/") . "/assets/dist/manifest.json";
