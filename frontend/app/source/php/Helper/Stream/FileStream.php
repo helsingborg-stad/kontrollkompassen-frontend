bd49bd159264d94c9e9a1515a7bd52f2b04d1332
MrSpex <mail@dannilsson.se>
2025-09-05 17:30:22 +0200

    🐛fix: get meta data from stream

--- frontend/app/source/php/Helper/Stream/FileStream.php
+++ frontend/app/source/php/Helper/Stream/FileStream.php
@@ -8,7 +8,6 @@ use Psr\Http\Message\StreamInterface;
 
 use \KoKoP\Interfaces\AbstractStream;
 
-use function \KoKoP\Utils\encodeRFC7230;
 
 class FileStream implements AbstractStream
 {
@@ -36,9 +35,11 @@ class FileStream implements AbstractStream
                 ]
             ]);
 
+            $env = str_contains($this->apiUrl, 'http://') ? 'slim' : 'mock';
+
             $this->stream = StreamFactory::createFromEnv(
-                $this->apiUrl === '' ? 'mock' : 'slim',
-                $this->apiUrl === '' ?: fopen($this->apiUrl, 'rb', false, $context)
+                $env,
+                fopen($env !== 'slim' ? 'php://temp' : $this->apiUrl, 'rb', false, $context)
             );
 
             return $this->stream;
@@ -47,26 +48,29 @@ class FileStream implements AbstractStream
         }
     }
 
-    public function getContentType(): string | bool
+    public function getContentType(): string
     {
-        return $this->stream->getMetadata('wrapper_data')['content-type'] ?? false;
+        return $this->_getHeaderData('content-type');
     }
 
-    public function getFilename(): string | bool
+    public function getFilename(): string
     {
-        $contentDisposition = $this->stream->getMetadata('wrapper_data')['content-disposition'];
-        if (!$contentDisposition) {
-            return false;
-        }
+        $contentDisposition = $this->_getHeaderData('content-disposition');
 
         $pattern = str_contains($contentDisposition, 'filename*=')
             ? '/filename\\*=UTF-8\'\'([^;\\r\\n]+)/i'
             : '/filename="([^"]+)"/i';
-
         preg_match($pattern, $contentDisposition, $matches);
 
         return isset($matches[1])
-            ? encodeRFC7230($matches[1])
-            : false;
+            ? $matches[1]
+            : '';
+    }
+
+    private function _getHeaderData($key): string
+    {
+        $wrapperData = $this->stream->getMetadata('wrapper_data');
+        $header = array_find($wrapperData, fn($header) => str_starts_with(strtolower($header), strtolower($key) . ':'));
+        return $header ? trim(explode(':', $header, 2)[1]) : '';
     }
 }
